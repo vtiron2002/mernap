377de5cc9efa76d477dfb2f3bcf84d9151a84030
vtiron2002 <vtiron2002@gmail.com>
2020-05-29 13:40:42 -0400

    applied position relative with top: 50px; on dashboard

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,7 +8,7 @@ export default function Header({ usersName, userLoading }) {
 	}
 
 	return (
-		<nav className='navbar navbar-dark bg-primary text-white'>
+		<nav className='navbar fixed-top navbar-dark bg-primary text-white'>
 			<div className='container d-flex justify-content-between align-items-center'>
 				<Link className='navbar-brand' to='/'>
 					MERNAP
